197e87154f041c733ececaa69ae59fa717cd544d
Zhang Lin <init-zhang@proton.me>
2026-03-04 15:47:03 +0000

    Remove deleted constant file import

--- scheduler/fcfs.py
+++ scheduler/fcfs.py
@@ -6,11 +6,6 @@ First Come, First Serve scheduler.
 """
 
 
-from cpu import *
-from memory import *
-from constants import *
-
-
 def start_process(reg, mem, binary):
     pid = mem[M_PROCESS_LIST]+1
     # Validate process limit and check for gaps
@@ -75,39 +70,3 @@ def hexdump(src):
         print("\033[7m" if i % 32 == 0 else "\033[0m", end="")
         print(f"{padded_hex(i)}-{padded_hex(i+7)}:", " ".join(padded_hex(src[i]) for i in range(i, i+8)))
     print("\033[0m", end="")
-
-memory = init_memory()
-registers = init_cpu()
-
-from assembler import *
-
-with open("fib.tasm", "r") as f:
-    program = f.read()
-
-assembled = assemble(program)
-
-start_process(registers, memory, assembled)
-dequeue(registers, memory)
-load_process(registers, memory)
-hexdump(memory)
-hexdump(registers)
-
-while 1:
-    user = input("> ")
-
-    if user == "n":
-        print(f"{memory[M_CURRENT_PID] = }")
-        cpu_cycle(registers, memory)
-
-    elif user == "hex":
-        hexdump(memory)
-        hexdump(registers)
-
-    elif user == "rex":
-        hexdump(registers)
-
-    elif user == "mex":
-        hexdump(memory)
-
-    elif user == "q":
-        break
